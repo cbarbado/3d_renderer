724ec2c337d735459048406a33abc5b9cfaf67b7
Cesar <barbado@barbado.com.br>
2023-07-15 00:43:41 +0200

    code cleanse + poly fill simplification + zbuffer glitch fixed

--- 3DRenderer.js
+++ 3DRenderer.js
@@ -6,7 +6,7 @@ class Graphics {
       this.canvas.setAttribute("id", elementID);
       document.getElementById(divID).appendChild(this.canvas);
       if(typeof G_vmlCanvasManager != "undefined") {
-         canvas = G_vmlCanvasManager.initElement(canvas);
+         this.canvas = G_vmlCanvasManager.initElement(canvas);
       }
 
       this.context = this.canvas.getContext("2d");
@@ -27,7 +27,7 @@ class Graphics {
    }
    
    clearZbuffer() {
-      for(var i = 0; i< this.zBuffer.length; i++) {
+      for(let i = 0; i< this.zBuffer.length; i++) {
          this.zBuffer[i] = -3.40282347e+38;  // Z axis decreases with distance.
       }   
    }
@@ -35,23 +35,52 @@ class Graphics {
    drawPoligon(vertexes, color) {
       this.context.beginPath();
       this.context.moveTo(vertexes[0].x,vertexes[0].y);
-      for(var i = (vertexes.length - 1); i >= 0; i--) {
+      for(let i = (vertexes.length - 1); i >= 0; i--) {
          this.context.lineTo(vertexes[i].x,vertexes[i].y);
       }
       this.context.closePath();
       this.context.strokeStyle = color;
       this.context.stroke();
    }
-   
-   // BUG: the junction of the 2 triangles sometimes has a flaw that is not filled, probable related to rounding in the setPixel function
+
+   fillHalf(v1, v2, v3, c) {
+      let point = new Element3D();
+
+      v1 = v1.getRounded();
+      v2 = v2.getRounded();
+      v3 = v3.getRounded();
+
+      let d1 = v2.getSubtraction(v1);
+      let d2 = v3.getSubtraction(v1);
+
+      if(d1.y == 0 || d2.y == 0) {
+         return;
+      }
+
+      let stepY = (v1.y > v2.y) ? -1 : 1;
+      for (point.y = v1.y; point.y != (v2.y + stepY); point.y += stepY) {
+         let pos_x1 = Math.round(v1.x + (d1.x * ((point.y-v1.y)/d2.y)));
+         let pos_x2 = Math.round(v1.x + (d2.x * ((point.y-v1.y)/d2.y)));
+
+         let pos_z1 = v1.z + (d1.z * ((point.y-v1.y)/d1.y));
+         let pos_z2 = v1.z + (d2.z * ((point.y-v1.y)/d2.y));
+
+         let stepX = (pos_x1 > pos_x2) ? -1 : 1;
+         let ratio = (pos_x2 == pos_x1) ? 0 : 1 / (pos_x2 - pos_x1);
+         for(point.x = pos_x1; point.x != (pos_x2 + stepX); point.x += stepX) {
+            point.z = pos_z1 + (pos_z2 - pos_z1) * ((point.x - pos_x1) * ratio);
+            this.setPixel(point,c);
+         }
+      }
+   }   
+
    fillPolygon(vertexes, color) // 3 or 4 vertexes only
    {
-      var top = 0;
-      var bot = 0;
-      var mid = 0;
+      let top = 0;
+      let bot = 0;
+      let mid = 0;
    
-      // DEBUG ZBUFFER ---> contar o numero de faces e linhas pra pintar de vermelho linha que eu quero debugar !!!
-      for(var i = 1; i < 3; i++) {
+      for(let i = 1; i < 3; i++) {
          if(vertexes[i].y < vertexes[top].y) {
             top = i;
          }
@@ -64,65 +93,21 @@ class Graphics {
          mid ++;
       }
    
-      var delta1 = vertexes[top].getSubtraction(vertexes[bot]);
-      var delta2 = vertexes[top].getSubtraction(vertexes[mid]);
-      var delta3 = vertexes[mid].getSubtraction(vertexes[bot]);
-   
-      /* TOP -> MID */
-      var point = new Element3D();
-      for (point.y = vertexes[top].y; point.y <= vertexes[mid].y; point.y++) {
-         var pos_x1 = vertexes[top].x + (delta1.x * ((point.y-vertexes[top].y)/delta1.y));
-         var pos_x2 = vertexes[mid].x + (delta2.x * ((point.y-vertexes[mid].y)/delta2.y));
-   
-         var pos_z1 = vertexes[top].z + (delta1.z * ((point.y-vertexes[top].y)/delta1.y));
-         var pos_z2 = vertexes[mid].z + (delta2.z * ((point.y-vertexes[mid].y)/delta2.y));
-   
-         if(pos_x2 > pos_x1) {
-            var ratio = (pos_x2 == pos_x1) ? 0 : 1 / (pos_x2 - pos_x1);
-            for(point.x = pos_x1; point.x <= pos_x2; point.x++) {
-               point.z = pos_z1 + (pos_z2 - pos_z1) * ((point.x - pos_x1) * ratio);
-               this.setPixel(point,color);
-            }
-         }
-         else {
-            var ratio = (pos_x2 == pos_x1) ? 0 : 1 / (pos_x1 - pos_x2);
-            for(point.x = pos_x2; point.x <= pos_x1; point.x++) {
-               point.z = pos_z1 + (pos_z1 - pos_z2) * ((point.x - pos_x2) * ratio);
-               this.setPixel(point,color);
-            }
-         }
-      }
-   
-      /* MID -> BOT */
-      for (point.y = vertexes[mid].y; point.y <= vertexes[bot].y; point.y++) {
-         var pos_x1 = vertexes[top].x + (delta1.x * ((point.y-vertexes[top].y)/delta1.y));
-         var pos_x2 = vertexes[mid].x + (delta3.x * ((point.y-vertexes[mid].y)/delta3.y));
-   
-         var pos_z1 = vertexes[top].z + (delta1.z * ((point.y-vertexes[top].y)/delta1.y));
-         var pos_z2 = vertexes[mid].z + (delta3.z * ((point.y-vertexes[mid].y)/delta3.y));
-   
-         if(pos_x2 > pos_x1) {
-            var ratio = (pos_x2 == pos_x1) ? 0 : 1 / (pos_x2 - pos_x1);
-            for(point.x = pos_x1; point.x <= pos_x2; point.x++) {
-               point.z = pos_z1 + (pos_z2 - pos_z1) * ((point.x - pos_x1) * ratio);            
-               this.setPixel(point,color);
-            }
-         }
-         else {
-            var ratio = (pos_x2 == pos_x1) ? 0 : 1 / (pos_x1 - pos_x2);
-            for(point.x = pos_x2; point.x <= pos_x1; point.x++) {
-               point.z = pos_z1 + (pos_z1 - pos_z2) * ((point.x - pos_x2) * ratio);
-               this.setPixel(point,color);
-            }
-         }
-      }
-   
+      let delta1 = vertexes[top].getSubtraction(vertexes[bot]);
+      let delta2 = vertexes[top].getSubtraction(vertexes[mid]);
+      let delta3 = vertexes[mid].getSubtraction(vertexes[bot]);
+
+      let v3_x = Math.round(vertexes[top].x + (delta1.x * ((vertexes[mid].y-vertexes[top].y)/delta1.y)));
+      let v3_z = Math.round(vertexes[top].z + (delta1.z * ((vertexes[mid].y-vertexes[top].y)/delta1.y)));
+
+      this.fillHalf(vertexes[top], vertexes[mid], new Element3D(v3_x, vertexes[mid].y, v3_z), color);
+      this.fillHalf(vertexes[bot], vertexes[mid], new Element3D(v3_x, vertexes[mid].y, v3_z), color);
+  
       if(vertexes.length === 4) {
          this.fillPolygon(new Array(vertexes[2], vertexes[3], vertexes[0]), color);
       }
    }
 
-   // BUG: zbuffer still glithching for minor distances (remove culling to see it better) // check cube at 273 degress
    setPixel (point,color) {
       point = point.getRounded();
 
@@ -130,7 +115,7 @@ class Graphics {
          return;
       }
 
-      var offset = (point.y * this.canvas.width + point.x);
+      let offset = (point.y * this.canvas.width + point.x);
 
       if(this.zBuffer[offset] >= point.z) {
          return;
@@ -194,22 +179,22 @@ class Geometry3D {
 
       const camera = new Element3D(0,0,1500);
 
-      var teta = 60 * (Math.PI / 180); // tilt angle
-      var cos_teta = Math.cos(teta);
-      var sin_teta = Math.sin(teta);
+      let teta = 60 * (Math.PI / 180); // tilt angle
+      let cos_teta = Math.cos(teta);
+      let sin_teta = Math.sin(teta);
 
-      var cos_alpha = Math.cos(r);
-      var sin_alpha = Math.sin(r);
+      let cos_alpha = Math.cos(r);
+      let sin_alpha = Math.sin(r);
 
       // TODO: use transform matrix here instead of sepparate transformations, to increase performance
       this.vertices.forEach((v) => {
-         var p = new Element3D( // ROTATE ON Z - AZIMUTH
+         let p = new Element3D( // ROTATE ON Z - AZIMUTH
             (v.x * cos_alpha - v.y * sin_alpha) * s.x,
             (v.x * sin_alpha + v.y * cos_alpha) * s.y,
             v.z * s.z
          );
 
-         var p2 = new Element3D( // ROTATE ON X - CAMERA TILT ANGLE
+         let p2 = new Element3D( // ROTATE ON X - CAMERA TILT ANGLE
             p.x,
             p.y * cos_teta + p.z * sin_teta,
             p.z * cos_teta - p.y * sin_teta // FASTER p.y * -sin_teta + p.z * cos_teta;
@@ -219,16 +204,13 @@ class Geometry3D {
       });
    }
 
-   // BUG: Check why zBuffer is glitching on cube and pyramid // Culling is OK. It glitches because of perspective distortion.
    calcFacelightening(face) {
-      var v1 = this.transformedVertices[face[0]].getSubtraction(this.transformedVertices[face[1]]); // first edge of this face
-      var v2 = this.transformedVertices[face[2]].getSubtraction(this.transformedVertices[face[1]]); // second edge of this face
+      let v1 = this.transformedVertices[face[0]].getSubtraction(this.transformedVertices[face[1]]); // first edge of this face
+      let v2 = this.transformedVertices[face[2]].getSubtraction(this.transformedVertices[face[1]]); // second edge of this face
 
-      var vCross = v1.getCrossProduct(v2); // face normal vector
+      let vCross = v1.getCrossProduct(v2); // face normal vector
       vCross.normalize(); // unity face normal vector
 
-      // return (vCross.x + vCross.y + vCross.z) * -0.57735; // simplification of normal and view vectors dot product (cos of the angle between normal and view (1, 1, -1) vectors)
-      // TODO: FACE CULLING AND FACE LIGHTENING ARE DIFFERENT!!!! 17.03.2023
       return -vCross.z; // simplification of normal and view vectors dot product (cos of the angle between normal and view (1, 1, -1) vectors)
    }
 
@@ -239,19 +221,20 @@ class Geometry3D {
 
    /* TODO: convert the data files from multiple edges polygons to tryangles meshes */
    render(graphics, flagShading = false) {
-      var coords    = [];
+      let coords    = [];
       const offsetX = graphics.canvas.width  / 2;
       const offsetY = graphics.canvas.height / 2;
-      var rgbColor  = this.getColorRGB();
+      let rgbColor  = this.getColorRGB();
 
       this.transformedVertices.forEach((v) => {
          coords.push(new Element3D(1000 * v.x / v.z + offsetX, 1000 * v.y / v.z + offsetY, 1000 * v.z)); // TODO: recall why I am multiplying vertexes by 1000. Maybe camera distance/projection?
       });
 
       this.faces.forEach((f) => {
-         var v = [];
-         for(var i = 0; i < f.length; i++) {
-            v.push(coords[f[i]]);
+         let v = [];
+
+         for(const vertice of f) {
+            v.push(coords[vertice]);
          }
 
          if(!flagShading) { // wireframes only
@@ -259,9 +242,9 @@ class Geometry3D {
             return;
          }
 
-         var faceLightening = this.calcFacelightening(f);
+         let faceLightening = this.calcFacelightening(f);
          if(faceLightening > 0) {
-            var faceColor = new Array();
+            let faceColor = new Array();
             faceColor.r = Math.floor(rgbColor.r * faceLightening);
             faceColor.g = Math.floor(rgbColor.g * faceLightening);
             faceColor.b = Math.floor(rgbColor.b * faceLightening);
@@ -283,13 +266,12 @@ const shapes = { // TODO: Change from 2 shapes arrays to a single array of shape
    )),
 };
 
-var shapeIndex      = null;
-var flagShading     = true;
-var shapeResolution = "high_poly";
-var graphics = null;
+let shapeIndex      = null;
+let flagShading     = true;
+let shapeResolution = "high_poly";
+let graphics = null;
 
-var angle = 0;
-// DEBUG var angle = 273;
+let angle = 0;
 function animate() { // TODO: pass function pointer as parameter to graphics init?
    if(shapeIndex == null) return;
 
